08461c82b22d3438842d2f660d0706ce1c18fb62
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2026-06-06 15:59:44 +0530

    start implementing auth-service with spring security

--- EMPLOYEE/src/main/java/com/employee/EMPLOYEE/client/AddressClient.java
+++ EMPLOYEE/src/main/java/com/employee/EMPLOYEE/client/AddressClient.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-
-@FeignClient(name = "addressClient" , url = "${address.service.url}")
+//there are two ways you can set up feign client by using name from eureka server like ADDRESS or
+// you can give a unique name but specify the url for address service in application properties
+//like address.service.url (check application.properties) and then use the url in @FeignClient(url = "${address.service.url}")
+//like in employee service
+@FeignClient(name = "ADDRESS")
 public interface AddressClient {
 
-    @GetMapping("/employee/{empId}")
+    @GetMapping("/api/v1/address/employee/{empId}")
     List<AddressDto> getAddressByEmployeeId(@PathVariable Long empId);
 }
